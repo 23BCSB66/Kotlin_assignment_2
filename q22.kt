a32e966ac0d1f1facb78484c139a3b84d65d211c
Asish Kumar Behera <cse.23bcsb66@silicon.ac.in>
2026-06-01 10:00:57 +0530

    Add ScreenState sealed class and renderUI function

---
+++ q22.kt
@@ -0,0 +1,13 @@
+sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Success(val data: String) : ScreenState()
+    data class Error(val errorMsg: String) : ScreenState()
+}
+
+fun renderUI(state: ScreenState) {
+    when (state) {
+        is ScreenState.Loading -> println("Loading...")
+        is ScreenState.Success -> println("Success: ${state.data}")
+        is ScreenState.Error -> println("Error: ${state.errorMsg}")
+    }
+}
